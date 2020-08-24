e70e26230c6e98cc359853a01c2bf565858a0b32
情雨随风 <876451336@qq.com>
2020-08-24 23:29:19 +0800

    配置@路径

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,14 @@
 const CracoLessPlugin = require('craco-less')
+const path = require('path')
+
+const resolve = dir => path.join(__dirname, dir)
 
 module.exports = {
+	webpack: {
+		alias: {
+			'@': resolve('src')
+		}
+	},
 	plugins: [
 		{
 			plugin: CracoLessPlugin,
