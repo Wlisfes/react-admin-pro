d4338c43af5c69ae8e4a9b567f11a94a3df54781
情雨随风 <876451336@qq.com>
2020-08-24 22:59:35 +0800

    项目初始化

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Button } from 'antd'
+import './App.less'
 
 function App() {
-	return <div className="App">App</div>
+	return (
+		<div className="App" style={{ padding: '40px' }}>
+			<Button>Click</Button>
+		</div>
+	)
 }
 
 export default App
