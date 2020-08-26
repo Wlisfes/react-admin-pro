40818117b7047d0f2f48135216acb63d0eb4eead
情雨随风 <876451336@qq.com>
2020-08-26 23:39:11 +0800

    默认Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,43 @@
 import React, { FC, Suspense } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Loadable from 'react-loadable'
 import { Spin } from 'antd'
+import Layout from '@/components/layout'
 import './style/App.less'
-import Home from './views/home/Home'
+
+const loadComponent = (name: string) =>
+	Loadable({
+		loader: () => import(`@/views/${name}`),
+		loading: () => (
+			<div
+				style={{
+					width: '100%',
+					height: '400px',
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center'
+				}}
+			>
+				<Spin size="large"></Spin>
+			</div>
+		)
+	})
 
 const App: FC = () => (
 	<Suspense fallback={<Spin size="large"></Spin>}>
 		<Router>
 			<Switch>
-				<Route path="/" component={Home}></Route>
+				<Route
+					path="/"
+					render={() => (
+						<Layout>
+							<Switch>
+								<Route path="/main" component={loadComponent('home/Home')} />
+								<Route component={loadComponent('home/Home')} />
+							</Switch>
+						</Layout>
+					)}
+				></Route>
 			</Switch>
 		</Router>
 	</Suspense>
